1a92c79916d10b24c4b2f137792c428f070d8cf5
Frostzeichen <cruztjomar@gmail.com>
2024-12-31 04:07:07 +0800

    test: return type and parameter

--- shell/shell_test.go
+++ shell/shell_test.go
@@ -2,17 +2,42 @@ package shell
 
 import (
 	"testing"
+	"reflect"
+	"encoding/json"
 )
 
 func TestRunGETPrices(t *testing.T) {
-	want := "BTC"
-	got := GETPrices("BTC")
+	t.Run("GETPrices returns price", func(t *testing.T) {
+		response := GETPrices("BTC", false)
+		var f PricesJSONResponse
+		json.Unmarshal([]byte(string(response)), &f)
+		got := reflect.TypeOf(f.Price)
+		want := reflect.TypeOf("1.001")
 
-	assertStatus(t, want, got)
+		assertStatus(t, got, want)
+	})
+
+	t.Run("parameter works", func(t *testing.T) {
+		var f PricesJSONResponse
+		var g PricesJSONResponse
+
+		response1 := GETPrices("BTC", true)
+		response2 := GETPrices("USDC", true)
+
+		json.Unmarshal([]byte(string(response1)), &f)
+		json.Unmarshal([]byte(string(response2)), &g)
+
+		got := f.Price
+		want := g.Price
+
+		if got == want {
+			t.Errorf("Seems like both values got the same value of %s.", got)
+		}
+	})
 }
 
-func assertStatus(t *testing.T, want string, got string) {
+func assertStatus(t *testing.T, got reflect.Type, want reflect.Type) {
 	if want != got {
-		t.Errorf("got %s expected %s", want, got)
+		t.Errorf("got %s expected %s", got, want)
 	}
 }
