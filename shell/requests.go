89dde508ff45c986741819c93b469d2b3af2931b
Frostzeichen <cruztjomar@gmail.com>
2024-12-31 14:44:28 +0800

    feat: add POSTTradeQuote

--- shell/requests.go
+++ shell/requests.go
@@ -15,7 +15,7 @@ type ExchangeInformationJSONResponse struct {
 	Timezone        string        `json:"timezone"`
 	ExchangeFilters []interface{} `json:"exchangeFilters"`
 	ServerTime      int           `json:"serverTime"`
-	Symbols         []struct {
+	Symbols         []struct { // TODO: Refactor to reduce indentation.
 		Symbol              string   `json:"symbol"`
 		Status              string   `json:"status"`
 		BaseAsset           string   `json:"baseAsset"`
@@ -23,7 +23,7 @@ type ExchangeInformationJSONResponse struct {
 		QuoteAsset          string   `json:"quoteAsset"`
 		QuoteAssetPrecision int      `json:quoteAssetPrecision"`
 		OrderTypes          []string `json:"orderTypes"`
-		Filters             []struct {
+		Filters             []struct { // TODO: Refactor to reduce indentation.
 		        FilterType        string `json:"filterType"`
 		        MinPrice          string `json:"minPrice,omitempty"`
 		        MaxPrice          string `json:"maxPrice,omitempty"`
@@ -48,6 +48,20 @@ type ExchangeInformationJSONResponse struct {
 
 }
 
+type TradeQuoteJSONResponse struct {
+	Status int    `json:"status"`
+	Error  string `json:"error"`
+	Data   struct {
+		QuoteID        string `json:"quoteId"`
+		SourceCurrency string `json:"sourceCurrency"`
+		TargetCurrency string `json:"targetCurrency"`
+		SourceAmount   string `json:"sourceAmount"`
+		TargetAmount   string `json:"targetAmount"`
+		Price          string `json:"price"`
+		Expiry         string `json:"expiry"`
+	} `json:"data"`
+}
+
 // Gets the latest price and returns raw JSON output.
 func GETPrices(currency string, printJSON bool) []byte {
 	cmd := exec.Command("./check_prices.sh", currency)
@@ -55,6 +69,19 @@ func GETPrices(currency string, printJSON bool) []byte {
 
 	if printJSON {
 		var f PricesJSONResponse
+		json.Unmarshal([]byte(string(cmdOutput)), &f)
+		fmt.Println(f)
+	}
+
+	return cmdOutput
+}
+
+func POSTTradeQuote(currency string, printJSON bool) []byte {
+	cmd := exec.Command("./fetch_quote.sh", currency)
+	cmdOutput, _ := cmd.Output()
+
+	if printJSON {
+		var f TradeQuoteJSONResponse
 		json.Unmarshal([]byte(string(cmdOutput)), &f)
 		fmt.Println(f)
 	}
